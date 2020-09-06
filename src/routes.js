e22ded9fd601fab3303d93bec051152691c7971b
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-07 05:15:27 +0530

    updating files

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, HashRouter, Switch } from "react-router-dom";
-import Home from "./views/Home";
+import About from './views/About';
+import Home from './views/Home';
+import Contact from './views/Contact';
 
 export default (props) => (
   <HashRouter>
-      <Switch>
-        <Route exact path="" component={Home} />{" "}
-      </Switch>{" "}
+    <Switch>
+      <Route exact path="/" component={Home} />{" "}
+      <Route exact path="/about" component={About} />{" "}
+      <Route exact path="/contact" component={Contact} />{" "}
+    </Switch>{" "}
   </HashRouter>
 );
